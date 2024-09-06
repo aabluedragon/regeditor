12018a075a94da26a947e64779112c46361d2280
Alon Amir <aabluedragon@gmail.com>
2024-09-06 20:26:42 +0300

    Note about (Default) possibly being a value name for a non-default value.

--- src/constants.ts
+++ src/constants.ts
@@ -14,4 +14,10 @@ export const TIMEOUT_DEFAULT = 30000;
 
 export const REG_TYPES_ALL = Object.freeze(['REG_BINARY', 'REG_DWORD', 'REG_EXPAND_SZ', 'REG_MULTI_SZ', 'REG_NONE', 'REG_QWORD', 'REG_SZ'] as RegType[])
 
+/**
+ * The default value for a key.
+ * 
+ * Note that we cannot differentiate between a value named "(Default)" and the default value of a key, as the REG QUERY command does not indicate which it is.  
+ * Therefore, we will always assume that the value is the default value of the key.
+ */
 export const REG_VALUE_DEFAULT = "(Default)"
