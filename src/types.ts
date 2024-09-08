0c04c1fd66fe9b4860b4cf42da6220d6ec3ae201
Alon Amir <aabluedragon@gmail.com>
2024-09-09 00:51:08 +0300

    REG_NONE: just a comment correction.

--- src/types.ts
+++ src/types.ts
@@ -30,7 +30,7 @@ export interface REG_BINARY {
 }
 
 /**
- * REG_NONE contains binary data, but the REG ADD command does not encode it properly, instead it treats it as a UTF8 string. if you need to add binary data, use .reg files instead (REG IMPORT).
+ * REG_NONE contains binary data, but the REG ADD command does not encode it properly, instead it treats it as a string. if you need to add binary data, use .reg files instead (REG IMPORT). if you use the regApply(...) function, it should work as expected.
  */
 export interface REG_NONE {
     type: 'REG_NONE';
